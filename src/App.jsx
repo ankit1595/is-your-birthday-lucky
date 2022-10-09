08d4158ae1b41d7eed0c679623ca8f37939ad8f8
Ankit <ankit.95sharma@gmail.com>
2022-10-09 14:40:44 +0530

    styles: updated styles and favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import twitterLogo from "./images/brand-logos/logo-twitter.svg";
+import githubLogo from "./images/brand-logos/logo-github.svg";
+import linkedinLogo from "./images/brand-logos/logo-linkedin.svg";
+import mailLogo from "./images/brand-logos/mail.svg";
+import angelLogo from "./images/brand-logos/angel-icon.svg";
+import { clear } from "@testing-library/user-event/dist/clear";
 
 function App() {
   const [dob, setDob] = useState("");
   const [luckyNumber, setLuckyNumber] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
-  const [isLucky, setIsLucky] = useState(false);
+  const [output, setOutput] = useState("");
+
+  function clearMessage() {
+    setOutput("");
+    setErrorMessage("");
+  }
 
   function handleDobInput(e) {
     console.log(e.target.value);
     setDob(e.target.value);
-    setErrorMessage("");
+    clearMessage();
   }
   function handleLuckyNumInput(e) {
-    setErrorMessage("");
+    clearMessage();
     setLuckyNumber(e.target.value);
   }
 
   function checkBirthdayLucky() {
-    setErrorMessage("");
+    clearMessage();
     console.log("dob, luckyNUmber: ", dob, luckyNumber);
     if (dob && luckyNumber > 0) {
       let sumOfDob = 0;
       dob.split("").forEach((num) => {
-        // console.log("num", parseInt(num));
         if (!isNaN(Number(num))) {
           sumOfDob = sumOfDob + Number(num);
         }
       });
       console.log(sumOfDob);
       if (sumOfDob % luckyNumber === 0) {
-        setIsLucky(true);
+        setOutput("Your Birthday is Lucky 🎊");
       } else {
-        setIsLucky(false);
+        setOutput("Afsos! your birthday is NOT lucky 🎃");
       }
     } else {
       setErrorMessage("Invalid Input");
@@ -43,8 +53,9 @@ function App() {
   function resetForm() {
     setDob("");
     setLuckyNumber("");
-    setErrorMessage("");
+    clearMessage();
   }
+
   return (
     <div className="App">
       <h1>Is your Birthday Lucky? 🤩</h1>
@@ -58,9 +69,73 @@ function App() {
         onChange={handleLuckyNumInput}
       />
       <button onClick={checkBirthdayLucky}>Check</button>
-      <button onClick={resetForm}>Reset</button>
-      <div style={{ color: "red" }}>{errorMessage}</div>
-      <div style={{ color: "green" }}>{isLucky?"Your Birthday is Lucky" : "Afsos! "}</div>
+      <button
+        style={{
+          backgroundColor: "#dc3545",
+          color: "white",
+          borderColor: "#dc3545",
+        }}
+        onClick={resetForm}>
+        Reset
+      </button>
+      <div className="output" style={{ color: "red" }}>{errorMessage}</div>
+      <div className="output" style={{ color: "green" }}>
+        {output}
+      </div>
+      <p>*We are not storing your data</p>
+      <footer className="footer">
+        <div className="footer-header">
+          © | 2022 |{" "}
+          <a
+            className="link"
+            href="http://ankit-sharma15.netlify.app"
+            target={"_blank"}>
+            Ankit Sharma
+          </a>{" "}
+        </div>
+        <ul className="list-non-bullet link-social">
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://twitter.com/ankit1595"
+              target="_blank">
+              <img src={twitterLogo} alt="twitter-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://github.com/ankit1595"
+              target="_blank">
+              <img src={githubLogo} alt="github-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://www.linkedin.com/in/ankit1595"
+              target="_blank">
+              <img src={linkedinLogo} alt="linkedin-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="mailto:ankit.95sharma@gmail.com"
+              target="_blank">
+              <img src={mailLogo} alt="mail-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://www.angel.co/u/ankit1595"
+              target="_blank">
+              <img src={angelLogo} alt="angel-logo" />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
